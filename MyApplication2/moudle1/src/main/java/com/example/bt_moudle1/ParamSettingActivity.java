8254a588b7a3d98ba66738cf4c21ad693b5e6c7b
chenhua <chenhua@baojia.com>
2017-10-12 19:24:09 +0800

    修改了参数设置

--- MyApplication2/moudle1/src/main/java/com/example/bt_moudle1/ParamSettingActivity.java
+++ MyApplication2/moudle1/src/main/java/com/example/bt_moudle1/ParamSettingActivity.java
@@ -1,15 +1,13 @@
 package com.example.bt_moudle1;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.chenhao.bluetoothlib.utils.ToastUtils;
-import com.squareup.okhttp.Request;
-import com.zhy.http.okhttp.OkHttpUtils;
-import com.zhy.http.okhttp.callback.StringCallback;
 
 /**
  * Created by admin on 2017/9/20.
@@ -17,51 +15,75 @@ import com.zhy.http.okhttp.callback.StringCallback;
 
 public class ParamSettingActivity extends BaseActivity implements View.OnClickListener{
 
-    private Button btnSave;
-    private Button btnCancle;
+    private TextView txtXueyaGao;
+    private TextView txtXueyaDi;
+    private TextView txtXueyaZhengchang;
+    private EditText edtShousuoya;
+    private TextView txtMoshiChuzhenfa;
+    private TextView txtMoshiTingzhenfa;
+    private TextView txtMoshiChuzhentingzhengfa;
+    private EditText edtShuzhangya;
+    private TextView textView;
+    private EditText txtExamShichang;
+    private EditText txtMaobolvValue;
+    private EditText txtZuidaPengzhangShuiping;
+    private CheckBox chbTingzhengJianxi;
+    private TextView txtMaiboQiangduThree;
+    private TextView txtMaiboQiangduTwo;
+    private TextView txtMaiboQiangduOne;
+    private TextView txtKeluofuteValueOne;
+    private TextView txtKeluofuteValueTwo;
+    private TextView txtKeluofuteValueThree;
+    private TextView txtKeluofuteValueFour;
+    private TextView txtKeluofuteValueFive;
+    private Button btnExamSave;
+    private Button btnEndExamCancle;
+
+    private void assignViews() {
+        txtXueyaGao = (TextView) findViewById(R.id.txt_xueya_gao);
+        txtXueyaGao.setOnClickListener(this);
+        txtXueyaDi = (TextView) findViewById(R.id.txt_xueya_di);
+        txtXueyaDi.setOnClickListener(this);
+        txtXueyaZhengchang = (TextView) findViewById(R.id.txt_xueya_zhengchang);
+        edtShousuoya = (EditText) findViewById(R.id.edt_shousuoya);
+        txtMoshiChuzhenfa = (TextView) findViewById(R.id.txt_moshi_chuzhenfa);
+        txtMoshiTingzhenfa = (TextView) findViewById(R.id.txt_moshi_tingzhenfa);
+        txtMoshiChuzhentingzhengfa = (TextView) findViewById(R.id.txt_moshi_chuzhentingzhengfa);
+        edtShuzhangya = (EditText) findViewById(R.id.edt_shuzhangya);
+        textView = (TextView) findViewById(R.id.textView);
+        txtExamShichang = (EditText) findViewById(R.id.txt_exam_shichang);
+        txtMaobolvValue = (EditText) findViewById(R.id.txt_maobolv_value);
+        txtZuidaPengzhangShuiping = (EditText) findViewById(R.id.txt_zuida_pengzhang_shuiping);
+        chbTingzhengJianxi = (CheckBox) findViewById(R.id.chb_tingzheng_jianxi);
+        txtMaiboQiangduThree = (TextView) findViewById(R.id.txt_maibo_qiangdu_three);
+        txtMaiboQiangduTwo = (TextView) findViewById(R.id.txt_maibo_qiangdu_two);
+        txtMaiboQiangduOne = (TextView) findViewById(R.id.txt_maibo_qiangdu_one);
+        txtKeluofuteValueOne = (TextView) findViewById(R.id.txt_keluofute_value_one);
+        txtKeluofuteValueTwo = (TextView) findViewById(R.id.txt_keluofute_value_two);
+        txtKeluofuteValueThree = (TextView) findViewById(R.id.txt_keluofute_value_three);
+        txtKeluofuteValueFour = (TextView) findViewById(R.id.txt_keluofute_value_four);
+        txtKeluofuteValueFive = (TextView) findViewById(R.id.txt_keluofute_value_five);
+        btnExamSave = (Button) findViewById(R.id.btn_exam_save);
+        btnEndExamCancle = (Button) findViewById(R.id.btn_end_exam_cancle);
+        btnExamSave.setOnClickListener(this);
+        btnEndExamCancle.setOnClickListener(this);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_param_setting);
-        initView();
-
-        OkHttpUtils
-                .post()
-                .url(Constant.GET_EXAM_STUDENT)
-                .addParams("ExamId", "49")
-                .addParams("stuId", "191")
-                .addParams("roomId", "14")
-                .addParams("empId", "54")
-                .build()
-                .execute(new StringCallback() {
-                    @Override
-                    public void onError(Request request, Exception e) {
-                        Log.e("tag===", "onError");
-                    }
-
-                    @Override
-                    public void onResponse(String response) {
-                        Log.e("tag===", "onResponse");
-                        Log.e("tag===", response);
-
-                    }
-                });
+        assignViews();
 
     }
 
-    private void initView() {
-        btnSave = (Button) findViewById(R.id.btn_exam_save);
-        btnSave.setOnClickListener(this);
-        btnCancle = (Button) findViewById(R.id.btn_end_exam_cancle);
-        btnCancle.setOnClickListener(this);
-    }
 
     @Override
     public void onClick(View view) {
-        if (view == btnSave){
+        if (view == btnExamSave){
             ToastUtils.showShort(this,"保存成功!");
-        }else if (view == btnCancle){
+        }else if (view == btnEndExamCancle){
             finish();
         }
     }
